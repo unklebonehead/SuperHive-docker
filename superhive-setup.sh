6de9c7092719857bc9d0033ed76fbd5f24010588
Unkle Bonehead <unklebonehead@gmail.com>
2023-09-11 21:25:33 -0400

    Update superhive-setup.sh

--- superhive-setup.sh
+++ superhive-setup.sh
@@ -2,4 +2,6 @@
 
 tar -xvzf superhive.tar.gz -C /var/www
 mv nginx.conf /etc/nginx/nginx.conf
-# need to figure out how to restart nginx. This container does not have systemd. /etc/init.d/nginx restart and service.nginx restart both fail.
+# need to figure out how to restart nginx. This container does not have systemd. /etc/init.d/nginx restart and service.nginx restart both fail. Try reboot command
+reboot
+
